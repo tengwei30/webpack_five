21d7ffce48ca050082cf7c174d3d23212b78aa5d
张嵩 <zhangsong@yufu.cn>
2021-08-17 18:10:39 +0800

    feat: 删除eslint 中的standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:vue/essential', 'standard'],
+    extends: ['eslint:recommended', 'plugin:vue/essential'],
     env: {
         browser: true,
         commonjs: true,
