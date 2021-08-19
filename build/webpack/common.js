1a4329d5a1cab14ec015c5de0851047622daf3f6
张嵩 <zhangsong@yufu.cn>
2021-08-19 15:23:49 +0800

    feat: 添加ts 配置

--- build/webpack/common.js
+++ build/webpack/common.js
@@ -1,24 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader/dist/index');
-const paths = require("../paths");
+const paths = require('../paths');
 
 // 获取路径文件
-const resolve = (dir) => {
-  return path.join(__dirname, "../..", dir);
-}
+const resolve = dir => {
+  return path.join(__dirname, '../..', dir);
+};
 
 const isProductionMode = process.env.NODE_ENV === 'production';
 // 开发模式 style-loader, 生产模式 minicss
 const styleLoaderMode = isProductionMode ? MiniCssExtractPlugin.loader : 'style-loader';
 // 关闭 ts 的类型检查，异步到插件去做，能提升打包速度
-const babelOptions = {transpileOnly: true};
+const babelOptions = { transpileOnly: true };
 
 const commonConfig = {
-  target: "web",
+  target: 'web',
   entry: {
     polyfills: resolve('src/polyfills'), // 对不支持ES6的浏览器进行支持
     index: paths.appIndexJs
@@ -26,18 +26,20 @@ const commonConfig = {
   output: {
     path: paths.appBuild,
     filename: 'js/[id].[contenthash].js',
-    publicPath: "/",
+    publicPath: '/',
     clean: true, // 清理 /dist 文件夹
     charset: true,
-    pathinfo: false,  // 输出结果不携带路径信息
+    pathinfo: false // 输出结果不携带路径信息
     // assetModuleFilename: 'images/[hash][ext][query]' // 等同于下面 generator 的配置，自定义输出路径
   },
-  cache: {  // 使用缓存
+  cache: {
+    // 使用缓存
     type: 'filesystem', // memory:使用内容缓存 filesystem：使用文件缓存
     allowCollectingMemory: true,
-    buildDependencies: {  // 当模块代码没有发生变化时，缓存失效
-      config: [__filename],
-    },
+    buildDependencies: {
+      // 当模块代码没有发生变化时，缓存失效
+      config: [__filename]
+    }
   },
   module: {
     rules: [
@@ -46,27 +48,25 @@ const commonConfig = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader", // vue 的需要在ts-loader 前面添加babel-loader，因为tsjson中 "jsx" 设置成了 "preserve", 。否则 ts不会被转成 js
+            loader: 'babel-loader' // vue 的需要在ts-loader 前面添加babel-loader，因为tsjson中 "jsx" 设置成了 "preserve", 。否则 ts不会被转成 js
           },
           {
-            loader: "ts-loader",
-            options: babelOptions,
-          },
-        ],
+            loader: 'ts-loader',
+            options: babelOptions
+          }
+        ]
       },
       {
         test: /\.vue$/,
-        use: [
-          'vue-loader'
-        ]
+        use: ['vue-loader']
       },
       {
         test: /\.css$/,
         use: [
           styleLoaderMode,
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader',
-        ],
+          'postcss-loader'
+        ]
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -85,27 +85,29 @@ const commonConfig = {
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'ts-loader',
-          options: babelOptions,
-        }]
+        use: [
+          {
+            loader: 'ts-loader',
+            options: babelOptions
+          }
+        ]
       },
       {
         test: /\.less$/,
         use: [
           styleLoaderMode,
           {
-            loader: 'css-loader',
+            loader: 'css-loader'
           },
           {
             loader: 'less-loader',
             options: {
               lessOptions: {
-                javascriptEnabled: true,
-              },
-            },
-          },
-        ],
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.scss$/,
@@ -118,16 +120,16 @@ const commonConfig = {
             }
           },
           'sass-loader',
-          'postcss-loader',
-        ],
-      },
+          'postcss-loader'
+        ]
+      }
     ]
   },
   resolve: {
     extensions: paths.moduleFileExtensions,
     alias: {
-      src: resolve(__dirname, '../../src'),
-      assets: resolve(__dirname, '../../assets'),
+      '@': resolve(__dirname, '../../src'),
+      assets: resolve(__dirname, '../../assets')
     }
   },
   plugins: [
@@ -136,20 +138,19 @@ const commonConfig = {
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: isProductionMode
-          ? `css/[id].[contenthash].css`
-          : `css/[path]/[name]_[hash:base64:5].css`,
+        ? `css/[id].[contenthash].css`
+        : `css/[path]/[name]_[hash:base64:5].css`,
       chunkFilename: isProductionMode
-          ? `css/[name].[contenthash].css`
-          : `css/[path]/[name]_[hash:base64:5].css`,
-      ignoreOrder: true,
+        ? `css/[name].[contenthash].css`
+        : `css/[path]/[name]_[hash:base64:5].css`,
+      ignoreOrder: true
     }),
     new HtmlWebpackPlugin({
       template: paths.appHtml,
       inject: 'body',
-      title: "Vue"
-    }),
+      title: 'Vue'
+    })
   ]
-}
+};
 
-module.exports = commonConfig
-
+module.exports = commonConfig;
